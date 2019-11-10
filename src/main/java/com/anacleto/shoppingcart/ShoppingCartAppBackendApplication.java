9f035a6c61f577e4fb3d8b0c7591101068e42d47
Tiago Anacleto <tianacleto1@gmail.com>
2019-11-10 09:36:54 -0300

    Initial project commit

---
+++ src/main/java/com/anacleto/shoppingcart/ShoppingCartAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anacleto.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anacleto/shoppingcart/ShoppingCartAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anacleto.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
